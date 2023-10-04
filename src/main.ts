57bdeb77fda104e3a16f71223380303942527b4c
Jan Michael Muñoz Botero <112509633+JMMB17@users.noreply.github.com>
2023-10-03 19:23:05 -0500

    final version without navbar

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {fas} from "@fortawesome/free-solid-svg-icons";
-library.add(fas);
 
-const app = createApp(App).component("fa", FontAwesomeIcon)
+const app = createApp(App)
 
 app.use(createPinia())
 
