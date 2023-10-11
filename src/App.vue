4138ec60310a8334a8a69d1cb044351a4a6eb0f3
Jan Michael Muñoz Botero <112509633+JMMB17@users.noreply.github.com>
2023-10-10 23:02:41 -0500

    Outdated imports deleted

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
     import {RouterView} from "vue-router";
     import Navbar from "./components/core/Navbar.vue";
-import DeleteProfileView from "./views/DeleteProfileView.vue";
 </script>
 
 <template>
     <div class="container">
             <Navbar/>
             <RouterView />
-            <!-- <DeleteProfileView/> -->
         </div>
 </template>
 
